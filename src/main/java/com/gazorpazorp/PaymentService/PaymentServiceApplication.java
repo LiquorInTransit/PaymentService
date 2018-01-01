4fa4615c50b5bc7be404745656410601e9e7492a
Christopher Peckover <christopher.peckover@gmail.com>
2018-01-01 17:45:18 -0500

    Initial commit again. sorry.

--- src/main/java/com/gazorpazorp/PaymentService/PaymentServiceApplication.java
+++ src/main/java/com/gazorpazorp/PaymentService/PaymentServiceApplication.java
@@ -1,4 +1,4 @@
-package com.gazorpazorp.SAMPLE_APPLICATION;
+package com.gazorpazorp.PaymentService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/gazorpazorp/controller/PaymentController.java
+++ src/main/java/com/gazorpazorp/controller/PaymentController.java
@@ -1,23 +1,15 @@
 package com.gazorpazorp.controller;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gazorpazorp.model.Sample;
-import com.gazorpazorp.model.dto.SampleMinimalDto;
-import com.gazorpazorp.model.dtoMapper.SampleMapper;
-import com.gazorpazorp.service.SampleService;
+import com.gazorpazorp.service.PaymentService;
 
 @RestController
 @RequestMapping("/api/payments")
@@ -27,13 +19,9 @@ public class PaymentController {
 	PaymentService paymentService;
 	
 	@GetMapping
-	public ResponseEntity<List<SampleMinimalDto>> getAll() throws Exception{
+	public ResponseEntity getAll() throws Exception{
 		return Optional.ofNullable(paymentService.getAllSamples())
-				.map(s -> new ResponseEntity<List<SampleMinimalDto>>
-						(s.stream()
-						.map(sample -> SampleMapper.INSTANCE.sampleToSampleMinimalDto(sample))
-						.collect(Collectors.toList()), HttpStatus.OK)
-					)
+				.map(s -> new ResponseEntity(s, HttpStatus.OK))
 				.orElseThrow(() -> new Exception("Account does not exist"));
 	}
 	
