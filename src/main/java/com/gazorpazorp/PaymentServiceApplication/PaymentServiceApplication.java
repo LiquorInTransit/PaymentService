72812ff1573ecd81119fb3d0e0884edcaf686827
Christopher Peckover <christopher.peckover@gmail.com>
2018-01-01 17:22:21 -0500

    Initial Commit

---
+++ src/main/java/com/gazorpazorp/PaymentServiceApplication/PaymentServiceApplication.java
@@ -0,0 +1,62 @@
+package com.gazorpazorp.SAMPLE_APPLICATION;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import com.gazorpazorp.client.config.CustomOAuth2FeignRequestInterceptor;
+import com.netflix.appinfo.AmazonInfo;
+
+import feign.RequestInterceptor;
+
+@SpringBootApplication(scanBasePackages="com.gazorpazorp")
+@EnableJpaRepositories("com.gazorpazorp.repository")
+@EntityScan(basePackages="com.gazorpazorp")
+@EnableEurekaClient
+@EnableFeignClients("com.gazorpazorp.client")
+@EnableResourceServer
+@EnableOAuth2Client
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+	
+	@Bean
+	@Profile("!dev")
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) 
+	{
+		EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+		instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+		instance.setDataCenterInfo(info);
+		instance.setNonSecurePort(8080);
+		return instance;
+	}
+	
+	@Bean
+	RequestInterceptor oauth2FeignRequestInterceptor(OAuth2ClientContext context) {
+		if (context == null) return null;
+		return new CustomOAuth2FeignRequestInterceptor(context);
+	}
+    
+    	
+//	@PostConstruct
+//	public void getDbManager(){
+//	   DatabaseManagerSwing.main(
+//		new String[] { "--url", "jdbc:hsqldb:mem:test://localhost/test?characterEncoding=UTF-8", "--user", "SA", "--password", ""});
+//	}
+}
